9e45193272cf818b1e480ca7dd7d0ee3f9e2f8b3
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2023-08-29 13:39:28 +0530

    changed font size of  nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -51,13 +51,13 @@ export const Navigation: React.FC = () => {
           <div className="flex text-teal-50 gap-5">
             <Link
               href="/department"
-              className="duration-200  text-teal-400 hover:text-teal-100"
+              className="duration-200 tiltneon text-xl text-teal-400 hover:text-teal-100"
             >
               Departments
             </Link>
             <Link
               href="/contact"
-              className="duration-200 text-teal-400 hover:text-teal-100"
+              className="duration-200 tiltneon text-xl text-teal-400 hover:text-teal-100"
             >
               Contact
             </Link>
